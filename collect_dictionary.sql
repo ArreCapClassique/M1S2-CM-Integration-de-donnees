c7cc2e0bc3284ff9e6ba725fb70d6199c9030d34
乃0你带我走吧 <57453310+ArreCapClassique@users.noreply.github.com>
2025-04-15 17:14:56 +0200

    add colomn "Type et format"

--- collect_dictionary.sql
+++ collect_dictionary.sql
@@ -2,13 +2,26 @@ SELECT
     table_name AS "Source de données",
     column_name AS "Colonne/Attribut",
     CASE
-        WHEN data_type='NUMBER'
+        WHEN data_type = 'NUMBER'
             THEN
                 CASE
-                    WHEN data_precision IS NOT NULL THEN 'NUMBER(' || data_length || ',' || data_precision || ')'
-                    ELSE 'NUMBER(' || data_length || ')'
+                    WHEN data_precision IS NULL THEN 'Nombre'
+                    WHEN data_scale IS NULL OR data_scale=0 THEN 'Nombre de ' || data_precision || ' chiffres'
+                    ELSE 'Nombre de ' || data_precision || ' chiffres, dont ' || data_scale || ' décimales'
                 END
-        WHEN data_type='VARCHAR2' THEN 'VARCHAR2(' || data_length || ')'
+        WHEN data_type = 'VARCHAR2' THEN 'Chaîne de ' || data_length || ' car.'
+        ELSE INITCAP(data_type)
+    END AS "Type et format",
+    CASE
+        WHEN data_type = 'NUMBER'
+            THEN
+                CASE
+                    WHEN data_precision IS NULL THEN 'NUMBER(38,0)'
+                    WHEN data_scale IS NULL OR data_scale=0 THEN 'NUMBER(' || data_precision || ',0)'
+                    ELSE 'NUMBER(' || data_precision || ',' || data_scale || ')'
+                END
+        WHEN data_type = 'VARCHAR2' THEN 'VARCHAR2(' || data_length || ')'
         ELSE data_type
     END AS "Type de la BD"
-FROM USER_TAB_COLUMNS;
+FROM USER_TAB_COLUMNS
+ORDER BY "Colonne/Attribut";
